ae54606b03c7fb011320a25847e9aab8577ee48e
Atom-gnomov <gorbach0864213579@gmail.com>
2024-12-25 23:26:34 +0200

    task completed

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         if self.distance_from_city_center == 0:
-            return None
+            return 0
         temp = self.average_rating / self.distance_from_city_center
         cost = car.comfort_class * (self.clean_power - car.clean_mark) * temp
         return round(cost, 1)
